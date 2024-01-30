585a0a1852b0a604f5821b714ac8480f1a394dc6
yuvrajraghuvanshi <yuvrajraghuvanshi070101@gmail.com>
2024-01-30 11:14:11 +0530

    any api added

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -308,5 +308,38 @@ const getData =async (req, res) => {
       })
   }
   }
+  const filteredByAny = async (req, res) => {
+    try {
+        const { search } = req.params;
+        if (search.length < 3) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid search",
+            })
+        }
+        const allData = await DataModel.find({
+            $or: [{ sector: { $regex: search, $options: 'i' } }, { topic: { $regex: search, $options: 'i' } },
+            { insight: { $regex: search, $options: 'i' } }, { title: { $regex: search, $options: 'i' } },
+            { pestle: { $regex: search, $options: 'i' } }, { source: { $regex: search, $options: 'i' } },
+            { url: { $regex: search, $options: 'i' } }]
+        });
+        if (!allData || allData.length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "No Data Found",
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: `Filtered by search ${search}`,
+            data: allData
+        })
+    } catch (e) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        })
+    }
+}
   
-  module.exports = { getData,filterByYear,filterByTopic,filterByTitle,filterByCountry,filterByRegion,filterBySector,filterByPestle,filterBySource,filterByIntensity,filterByLikelyhood };
+  module.exports = { getData,filterByYear,filterByTopic,filterByTitle,filterByCountry,filterByRegion,filterBySector,filterByPestle,filterBySource,filterByIntensity,filterByLikelyhood,filteredByAny };
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -1,4 +1,4 @@
-const { getData, filterByYear, filterByTopic, filterByTitle, filterByCountry, filterByRegion, filterBySector, filterByPestle, filterBySource, filterByLikelyhood, filterByIntensity } = require('../controllers/dataController')
+const { getData, filterByYear, filterByTopic, filterByTitle, filterByCountry, filterByRegion, filterBySector, filterByPestle, filterBySource, filterByLikelyhood, filterByIntensity, filteredByAny } = require('../controllers/dataController')
 const express=require('express')
 const router=express.Router()
 
@@ -13,6 +13,7 @@ router.get('/source/:source',filterBySource)
 router.get('/pestle/:pestle',filterByPestle)
 router.get('/likelihood/:likelihood',filterByLikelyhood)
 router.get('/intensity/:intensity',filterByIntensity)
+router.get("/any/:search", filteredByAny)
 
 
 module.exports=router
